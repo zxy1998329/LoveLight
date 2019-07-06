a8638da0f8c3a5a4aa3f793fe4a2d35735544f14
mmmmute <546029685@qq.com>
2019-07-06 10:56:56 +0800

    合并，解决冲突

--- src/main/java/com/loveLight/entity/AccountExtend.java
+++ src/main/java/com/loveLight/entity/AccountExtend.java
@@ -4,41 +4,32 @@ public class AccountExtend {
 	private String username;
 	private String trueName;
 	private String birthday;
-<<<<<<< HEAD
-	private String marry;
-	private String edu;
-=======
+
 	private String marry;		//结婚为Y 离婚为N  离异为L
 	private String edu;			//小学1 初中2 高中3 本科4 硕士5 博士6
->>>>>>> branch 'dev' of https://github.com/zxy1998329/LoveLight.git
+
 	private int salary;
 	private String phoneNum;
 	private String introduction;
-<<<<<<< HEAD
-	private String trueName;
-=======
+
 	public String getTrueName() {
 		return trueName;
 	}
 	public void setTrueName(String trueName) {
 		this.trueName = trueName;
 	}
->>>>>>> branch 'dev' of https://github.com/zxy1998329/LoveLight.git
 	public String getBirthday() {
 		return birthday;
 	}
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
-<<<<<<< HEAD
-=======
 	public String getMarry() {
 		return marry;
 	}
 	public void setMarry(String marry) {
 		this.marry = marry;
 	}
->>>>>>> branch 'dev' of https://github.com/zxy1998329/LoveLight.git
 	public String getEdu() {
 		return edu;
 	}
@@ -63,38 +54,17 @@ public class AccountExtend {
 	public void setIntroduction(String introduction) {
 		this.introduction = introduction;
 	}
-	public String getMarry() {
-		return marry;
-	}
-	public void setMarry(String marry) {
-		this.marry = marry;
-	}
-	public String getTrueName() {
-		return trueName;
-	}
-	public void setTrueName(String trueName) {
-		this.trueName = trueName;
-	}
 	@Override
 	public String toString() {
-<<<<<<< HEAD
-		return "AccountExtend [birthday=" + birthday + ", marry=" + marry + ", edu=" + edu + ", salary=" + salary
-				+ ", phoneNum=" + phoneNum + ", introduction=" + introduction + ", trueName=" + trueName + "]";
-=======
 		return "AccountExtend [username=" + username + ", trueName=" + trueName + ", birthday=" + birthday + ", marry="
 				+ marry + ", edu=" + edu + ", salary=" + salary + ", phoneNum=" + phoneNum + ", introduction="
 				+ introduction + "]";
->>>>>>> branch 'dev' of https://github.com/zxy1998329/LoveLight.git
 	}
-<<<<<<< HEAD
-	
-=======
 	public String getUsername() {
 		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
 	}
->>>>>>> branch 'dev' of https://github.com/zxy1998329/LoveLight.git
 	
 }
